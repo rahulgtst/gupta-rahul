5fc86bcc983a2b2f1474c36a474aa60db83802d7
Rahul Gupta <rahulgtst@gmail.com>
2025-09-07 00:48:24 +0530

    Restructure Skill Constant and update skill section

--- src/constants/Skills.tsx
+++ src/constants/Skills.tsx
@@ -1,24 +1,24 @@
 import { FaHtml5, FaCss3Alt, FaJs, FaReact, FaNodeJs, FaAngular, FaAws, FaJava } from 'react-icons/fa';
-import { SiGit, SiGithub, SiTypescript, SiExpress, SiMongodb, SiMysql } from 'react-icons/si';
+import { SiGit, SiGithub, SiTypescript, SiExpress, SiMongodb } from 'react-icons/si';
 import { FaGolang } from "react-icons/fa6";
 import { FcLinux } from 'react-icons/fc';
-import { Skill } from '../types/Skill.ts';
+import { BiLogoPostgresql } from "react-icons/bi";
 
-export const SkillsData: Skill[] = [
-    { id: 1, name: 'HTML', icon: <FaHtml5 className="text-[#E34F26] text-5xl" /> },
-    { id: 2, name: 'CSS', icon: <FaCss3Alt className="text-[#1572B6] text-5xl" /> },
-    { id: 3, name: 'JavaScript', icon: <FaJs className="text-[#F7DF1E] text-5xl" /> },
-    { id: 4, name: 'TypeScript', icon: <SiTypescript className="text-[#3178C6] text-5xl" /> },
-    { id: 5, name: 'React', icon: <FaReact className="text-[#61DAFB] text-5xl" /> },
-    { id: 6, name: 'Angular', icon: <FaAngular className="text-[#DD0031] text-5xl" /> },
-    { id: 7, name: 'Node.js', icon: <FaNodeJs className="text-[#339933] text-5xl" /> },
-    { id: 8, name: 'Express', icon: <SiExpress className="text-[#000000] text-5xl" /> },
-    { id: 9, name: 'MongoDB', icon: <SiMongodb className="text-[#47A248] text-5xl" /> },
-    { id: 10, name: 'Java', icon: <FaJava className="text-[#ED8B00] text-5xl" /> },
-    { id: 11, name: 'Go', icon: <FaGolang className="text-[#00ADD8] text-5xl" /> },
-    { id: 12, name: 'SQL', icon: <SiMysql className="text-[#4479A1] text-5xl" /> },
-    { id: 13, name: 'Git', icon: <SiGit className="text-[#F05032] text-5xl" /> },
-    { id: 14, name: 'GitHub', icon: <SiGithub className="text-[#181717] text-5xl" /> },
-    { id: 15, name: 'Linux', icon: <FcLinux className="text-5xl" /> },
-    { id: 16, name: 'AWS', icon: <FaAws className="text-[#FF9900] text-5xl" /> },
-];
+export const SkillIcons: Record<string, JSX.Element> = {
+    HTML: <FaHtml5 className="text-[#E34F26]" />,
+    CSS: <FaCss3Alt className="text-[#1572B6]" />,
+    JavaScript: <FaJs className="text-[#F7DF1E]" />,
+    TypeScript: <SiTypescript className="text-[#3178C6]" />,
+    React: <FaReact className="text-[#61DAFB]" />,
+    Angular: <FaAngular className="text-[#DD0031]" />,
+    Node: <FaNodeJs className="text-[#339933]" />,
+    Express: <SiExpress className="text-[#000000]" />,
+    MongoDB: <SiMongodb className="text-[#47A248]" />,
+    Java: <FaJava className="text-[#ED8B00]" />,
+    Go: <FaGolang className="text-[#00ADD8]" />,
+    SQL: <BiLogoPostgresql className="text-[#4479A1]" />,
+    Git: <SiGit className="text-[#F05032]" />,
+    GitHub: <SiGithub className="text-[#181717]" />,
+    Linux: <FcLinux className="text-3xl" />,
+    AWS: <FaAws className="text-[#FF9900]" />,
+};
